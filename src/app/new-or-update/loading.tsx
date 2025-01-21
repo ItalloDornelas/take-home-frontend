e9ab2fba45cadbfac7c99b60ac888bb58fbe6b0d
Itallo <dornelaszxd@gmail.com>
2025-01-21 11:42:38 -0300

    fix: force to use a dark mode

--- src/app/new-or-update/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingComponent } from "@/components/ui/loading";
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center">
-      <LoadingComponent />
-    </div>
-  );
-}
