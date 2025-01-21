9520c7af1825b62855ca1144b3031b9fc0db2f1e
Itallo <dornelaszxd@gmail.com>
2025-01-21 11:34:30 -0300

    fix: loading in page

---
+++ src/app/new-or-update/loading.tsx
@@ -0,0 +1,9 @@
+import { LoadingComponent } from "@/components/ui/loading";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center">
+      <LoadingComponent />
+    </div>
+  );
+}
